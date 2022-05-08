2f68b34c000bf710a5e06b02389b01546426fc7f
Snitrxm <andre.oliveira.roch@hotmail.com>
2022-05-08 20:36:50 -0300

    Change routes

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const User = require('./models/user');
+var cors = require('cors');
 require('./config/database');
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+app.use(cors());
 
-app.post('/all', async (req, res) => {
+
+app.post('/user/all', async (req, res) => {
     const user = await User.find();
     res.json(user);
 })
 
-app.post('/register', async (req, res) => {
+app.post('/user/register', async (req, res) => {
     const { name, email, password } = req.body;
 
     try{
@@ -24,7 +27,7 @@ app.post('/register', async (req, res) => {
 })
 
 
-app.post('/login', async (req, res) => {
+app.post('/user/login', async (req, res) => {
     const { email, password } = req.body;
     
     try{
@@ -32,7 +35,7 @@ app.post('/login', async (req, res) => {
         if(!user){
             return res.status(400).json({err: 'User not found'});
         }else{
-            user.isCorrectPassword(password, function(same){
+            user.isCorrectPassword(password, function(err, same){
                 if(!same){
                     return res.status(400).json({err: 'Incorrect password'});
                 } else{
@@ -46,7 +49,7 @@ app.post('/login', async (req, res) => {
 })
 
 
-app.delete('/delete/:id', async (req, res) => {
+app.delete('/user/delete/:id', async (req, res) => {
     const _id = req.params.id;
     try{
         await User.findByIdAndRemove({_id});
